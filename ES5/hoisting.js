e38846b608a14ed337bbd2f96c12dbeac6e9b510
Itzik Cohen Arazi <itzik.ca@gmail.com>
2017-05-07 16:28:59 +0200

    Update hoisting.js

--- ES5/hoisting.js
+++ ES5/hoisting.js
@@ -1,13 +1,20 @@
 var a = 2;
 
-foo(); //foo decleration is hoisted
+foo(); //foo decleration is hoisted so we can run it
 
 function foo() {
-	a = 3;
+	a = 3; //declared later, so there's no leaking to global scope
 
 	console.log(a);	//3
 
-	var a; //decleration is hoisted to the top of foo
+	var a; //decleration is hoisted to the top of foo - so it doesnt leak to the global scope
 }
+
+bar; //undefined - bar is declared but its still undefined
+bar(); //error - undefined is not a function
+
+var bar = foo; //the decleration is hoisted but the assigining remains here.
+
+bar(); //3
 
 console.log(a);	// 2
