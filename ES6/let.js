747d4bc9bcf6327c0779b511aa0f0367c4fdfbbb
Itzik Cohen Arazi <itzik.ca@gmail.com>
2017-05-19 12:32:34 +0200

    Update let.js

--- ES6/let.js
+++ ES6/let.js
@@ -12,3 +12,16 @@ let b = 2;
   let b = 3;
 }
 console.log(b); //2
+
+//another interesting case is closure using let
+//consider a for loop with let instead of var
+var funcs = [];
+
+for (let i = 0; i < 5; i++) {
+	funcs.push( function(){
+		console.log( i );
+	} );
+}
+
+funcs[3](); //3 //using a 'var' it should have been 5...
+
