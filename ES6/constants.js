85550445e9f3bda11b0869f4312e947afa27ae11
Itzik Cohen Arazi <itzik.ca@gmail.com>
2017-05-19 12:37:25 +0200

    Update constants.js

--- ES6/constants.js
+++ ES6/constants.js
@@ -1,1 +1,16 @@
-//add examples for the use of const
+{ //using constants
+	const a = 2;
+	console.log( a );	// 2
+
+	a = 3;				// TypeError!
+}
+
+{ //an array constant
+	const a = [1,2,3];
+	a.push( 4 );
+	console.log( a );		// [1,2,3,4]
+
+	a = 42;					// TypeError!
+} //we can change the object using its methods, we just cannot reassign it...
+
+
