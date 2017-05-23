1df9f728ea978bb0559bced6dbbdf0a9d60f8619
Itzik Cohen Arazi <itzik.ca@gmail.com>
2017-05-23 12:25:28 +0200

    Update classes.js

--- ES6/classes.js
+++ ES6/classes.js
@@ -1,5 +1,21 @@
 
-//consice methods
+//consice methods and properties
+
+var z = 5, w = 10;
+
+var o = {
+  x: function() {
+    console.log('regular method');
+  },
+  
+  y() {
+    console.log('consice method');
+  },
+  
+  z: z, //regular property
+  w //consice property
+
+}
 
 //the class
 
