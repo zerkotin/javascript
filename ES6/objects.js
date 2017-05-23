dd79003b571797fa942c2433e55fcbf71188b34b
Itzik Cohen Arazi <itzik.ca@gmail.com>
2017-05-23 12:33:43 +0200

    Create objects.js

---
+++ ES6/objects.js
@@ -0,0 +1,19 @@
+//consice methods and properties
+
+var z = 5, w = 10;
+
+var o = {
+  x: function() {
+    console.log('regular method');
+  },
+  
+  y() {
+    console.log('consice method');
+  },
+  
+  z: z, //regular property
+  w //consice property
+
+}
+
+//TODO Object.assign
