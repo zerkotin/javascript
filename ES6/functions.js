3944f8789e8ad57360c244d16f04c1101d78586c
Itzik Cohen Arazi <itzik.ca@gmail.com>
2017-05-23 12:19:13 +0200

    Update functions.js

--- ES6/functions.js
+++ ES6/functions.js
@@ -1,7 +1,37 @@
-//TODO default parameters example
+//default parameters example
+function foo(x = 10, y = 5) {
+  console.log(x, y);
+}
+foo(); //10, 5
+
+
+//function parameters destructing
+function foo({x}) { //object destruction
+  console.log(x);
+}
+foo({x:5}); // 5
+
+function foo([x]) { //array destruction
+  console.log(x);
+}
+foo([5]); // 5
+
+//function parameters destructing and default value
+function foo({x = 10} = {}) {
+  console.log(x);
+}
+foo(); //10
 
 //arrow functions example
-(a) => console.log(a)
+var foo = (a) => console.log(a);
+foo(2); //2
 
-//TODO arrow functions scope
+//arrow functions scope
+function Counter() { //a counter that will count every second
+  this.counter = 0;
+  
+  setInterval((function(){this.counter++}).bind(this), 1000); //we need bind.., otherwise this.counter++ will work on Window instead of the Counter instance
+  setInterval( () => this.counter++, 1000); //we dont need bind, arrow function are bounded to this automatically
+}
+
 
