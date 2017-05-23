52bd1a7f79fb40ad493f42ee40f287ec78f47fca
Itzik Cohen Arazi <itzik.ca@gmail.com>
2017-05-23 12:41:58 +0200

    Update objects.js

--- ES6/objects.js
+++ ES6/objects.js
@@ -1,5 +1,4 @@
 //consice methods and properties
-
 var z = 5, w = 10;
 
 var o = {
@@ -16,4 +15,18 @@ var o = {
 
 }
 
+//computed property names
+var prefix = 'something';
+var o = {
+  [prefix + 's']: function() {
+    console.log('look at that');
+  },
+  [prefix + 'x']() {
+    console.log('consice');
+  }
+}
+o.somethings(); //"look at that"
+o.somethingx(); //"consice"
+
 //TODO Object.assign
+//TODO generators
