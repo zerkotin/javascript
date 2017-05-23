417128449bfef3581568377eef6382ec07142d65
Itzik Cohen Arazi <itzik.ca@gmail.com>
2017-05-23 16:50:21 +0200

    Update objects.js

--- ES6/objects.js
+++ ES6/objects.js
@@ -28,4 +28,34 @@ var o = {
 o.somethings(); //"look at that"
 o.somethingx(); //"consice"
 
-//TODO Object.assign
+//super
+var o1 = {
+  x: 1,
+  foo() {
+    console.log(this.x);
+  }
+}
+
+var o2 = {
+  foo() { //super is only allowed in concise functions
+    this.x = 2;
+    super.foo(); //using super - cannot call super() only super.function!
+  }
+}
+Object.setPrototypeOf( o2, o1 ); //setting the prototype
+o2.foo(); //2
+
+//Object.assign
+var options1 = {
+  config1: 'someconfig',
+  config2: 'someotherconfig',
+  config3: 'overriden'
+}
+
+var options2 = {
+  config3: 'anotherconfig'
+}
+
+var options3 = Object.assign({}, options1, options2); //target, source, source...
+options3; //config1: someconfig, config2: someotherconfig, config3: anotherconfig
+
