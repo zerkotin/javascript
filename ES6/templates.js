a5ae246b02ceac460df9e52d03e0edfc9ae9762a
Itzik Cohen Arazi <itzik.ca@gmail.com>
2017-05-24 09:50:47 +0200

    Update templates.js

--- ES6/templates.js
+++ ES6/templates.js
@@ -1,3 +1,4 @@
+//using variables
 var x = 'moshe';
 
 console.log(`${x} is my name
@@ -6,12 +7,14 @@ several lines too...
 ${x} see!
 `); 
 
+//using functions
 console.log(`${x.toUpperCase()}`); //"MOSHE"
 
 function foo(something) {
   return something.toLowerCase();
 }
 
+//calling functions with parameters
 console.log(`${foo("MOSHE")}`); //"moshe"
 
 
