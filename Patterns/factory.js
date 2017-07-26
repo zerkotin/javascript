e5b533d9df2411f52245a68e9adf3d43d52bac7c
Itzik Cohen Arazi <itzik.ca@gmail.com>
2017-07-26 18:43:58 +0200

    Update factory.js

--- Patterns/factory.js
+++ Patterns/factory.js
@@ -1,1 +1,3 @@
-//the factory pattern
+//in general i dont see much use for the factory pattern, but in case you insist, its essentially a singleton that returns new instances
+
+//TODO provide and example
