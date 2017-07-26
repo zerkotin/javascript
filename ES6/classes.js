a4c9317c7c7d2d4b1854b4ffaa866d2b6dc15dea
Itzik Cohen Arazi <itzik.ca@gmail.com>
2017-07-26 17:40:03 +0200

    Update classes.js

--- ES6/classes.js
+++ ES6/classes.js
@@ -1,6 +1,24 @@
+//a person class
+class Person { //the class keyword
+  constructor(name) { //a constructor
+    this.name = name; //note the use of this
+  }
+  
+  sayHi() { //a function
+    console.log('Hi, my name is '+this.name);
+  }
+}
 
-//the class
+//a worker class that extends a person class
+class Worker extends Person { //the extends keyword
+  constructor(name, company) {
+    super(name); //calling super...
+    
+    this.company = company;
+  }
+  
+  whereDoYouWork() {
+    console.log('i work at: '+this.company);
+  }
+}
 
-//super
-
-//constructor
