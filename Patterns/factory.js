6ffbf7824e543bc1b94a195f9004e47f3314a4b4
Itzik Cohen Arazi <itzik.ca@gmail.com>
2017-08-17 17:21:55 +0200

    Update factory.js

--- Patterns/factory.js
+++ Patterns/factory.js
@@ -1,3 +1,37 @@
-//in general i dont see much use for the factory pattern, but in case you insist, its essentially a singleton that returns new instances
+//Config.js
+//we have a basic controller object
+const controller = {
+  path: null,
+  getUrl(id) {
+    if(id && this.path.indexOf(':id') !== -1) {
+      return this.path.replace(':id', id); //replace the placeholder with the id
+    }
+    else if(id && this.path.indexOf(':id') === -1){ //didnt expect id but received one
+      throw new Error('did not expect id');
+    }
+    else if(!id && this.path.indexOf(':id') !== -1) { //expected id but didn't receive one
+      throw new Error('expected id');
+    }
+    else {
+      return this.path;
+    }
+  }
+}
 
-//TODO provide and example
+//factory for controllers
+function createController(url) {
+  return Object.assign(Object.create(controller), {url});
+}
+
+//now lets use it
+const controllers = {
+  users: createController('/users'),
+  user: createController('/users/:id') //this one is expecting id
+}
+
+export default {controllers};
+
+//index.js
+import Config from './Config.js';
+
+fetch(Config.controllers.user.getUrl(1)).then(res => console.log(res)); //get user with id 1
