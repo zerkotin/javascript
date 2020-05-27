57674b668f387a618ff4160e9cc6c273c04625d0
Itzik Cohen Arazi <itzik.ca@gmail.com>
2020-05-27 17:26:36 +0200

    Update objects.js

--- ES5/objects.js
+++ ES5/objects.js
@@ -13,8 +13,9 @@ obj['b'];	//42
 obj['c'];	//true
 
 ///we can also add or change existing Objects
-// the Number type is an Object and we can add or change its functionality
-// what we are doing here is called polyfilling - note that this is only an example and not a real pollyfill
+//the Number type is an Object and we can add or change its functionality
+//what we are doing here is called polyfilling
+//note that this is only an example and not a real pollyfill or how you should do it
 if (!Number.isNaN) {
 	//we should pollyfill directly on prototype - we will learn more about prototypes in the prototype section
 	Number.isNaN = function isNaN(x) {
