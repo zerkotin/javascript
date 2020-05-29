2d4be7ae2943b67454aa02637d9764b771962643
Itzik Cohen Arazi <itzik.ca@gmail.com>
2020-05-29 22:43:50 +0200

    Update equality.js

--- ES5/equality.js
+++ ES5/equality.js
@@ -1,14 +1,12 @@
 var a = '42';
 var b = 42;
 
+//== will work with coercion
+//=== will work without coercion and will compare the raw references
 a == b; //true
 a === b; //false
 
-//== will work with coercion
-//=== will work without coercion and will compare the raw references
-
 ///in addition
-
 a > b;
 a < b;
 
@@ -17,3 +15,17 @@ a <= b;
 
 a != b; //with coercion
 a !== b; //without coercion
+
+//more interesting facts
+false == 0; //true -- same goes for empty string ''
+false === 0; //false -- same goes for empty string ''
+
+'' == false; //true -- same with 0
+'' === false; //false -- same with 0
+
+//but funny enough not this
+false == undefined; //false
+0 == null; //false
+
+//it seems that undefined and null are not coerced 
+
